0853f5ceaee4005602586377145505086545bb3d
Praveen <pkchmits@gmail.com>
2020-09-16 05:21:21 +0530

    1. View Binding added 2. setSupportActionBar used to the toolbar to behaving as actionBar for the Activity Window 3. Set Text for each tab 4. Set the tabs to fill entire gravity

--- app/src/main/java/com/sabkayar/praveen/tabexperiment/MainActivity.java
+++ app/src/main/java/com/sabkayar/praveen/tabexperiment/MainActivity.java
@@ -1,14 +1,36 @@
 package com.sabkayar.praveen.tabexperiment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
+import com.sabkayar.praveen.tabexperiment.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        //Set a toolbar to act as ActionBar for this Activity Window
+        setSupportActionBar(mBinding.toolbar);
+
+        // Create an instance of the tab layout from the view.--No need as we are using viewBinding
+
+        // Set the text for each tab.
+        mBinding.tabLayout.addTab(mBinding.tabLayout.newTab().setText(R.string.tab_label1));
+        mBinding.tabLayout.addTab(mBinding.tabLayout.newTab().setText(R.string.tab_label2));
+        mBinding.tabLayout.addTab(mBinding.tabLayout.newTab().setText(R.string.tab_label3));
+
+        // Set the tabs to fill the entire layout.
+        mBinding.tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // Use PagerAdapter to manage page views in fragments.
     }
 }
